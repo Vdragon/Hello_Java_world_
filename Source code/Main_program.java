e6e02a8444ac44de1f949b66a525b5682924ef54
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-12-17 16:16:58 +0800

    將主程式類別改名
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- Source code/Main_program.java
+++ Source code/Main_program.java
@@ -1,13 +1,13 @@
 /* 用「斜線、星號」、「星號、斜線」包住的區塊為多行註解 */
-/** \file Hello_Java_world_.java
+/** \file Main_program.java
     \brief 「Java 程式設計世界哈囉！」主程式
     \author Ｖ字龍(Vdragon)
     \copyright Ｖ字龍放棄此專案的一切權利，將其釋出到公有領域(public domain)。 */
 
-/** \brief Hello_Java_world_ 物件類別(class)
+/** \brief Main_program 物件類別(class)
 
-   每個 Java 來源程式碼檔案至少要有一個 public 物件類別 */
-public class Hello_Java_world_{
+    每個 Java 來源程式碼檔案至少要有一個 public 物件類別 */
+public class Main_program{
 	/** \brief public static void main() 為 Java 程式的進入點(entry point)子程式
 	 *  \param args 保存命令列參數的字串陣列，注意第一個命令列參數位於 0 索引值，本程式目前沒有實際用到此參數。 */
 	public static void main(String[] args){
