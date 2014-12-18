083ac656cb13b2df4bdfa3e00dff2bbe0fdc80fa
Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>
2014-12-19 01:16:28 +0800

    相容 JavaDoc 文件產生器
    Signed-off-by: Ｖ字龍(Vdragon) <Vdragon.Taiwan@gmail.com>

--- Documentation/Application Programming Interface(API) documentation/README.md
+++ Documentation/Application Programming Interface(API) documentation/README.md
@@ -3,5 +3,7 @@
 ## 本目錄下的項目說明<br />Description of the items under this directory
 ### [Doxygen/](Doxygen/)
 
+### [JavaDoc/](JavaDoc/)
+
 ### [說明文件.md<br />README.md](README.md)
 本說明文件
--- Source code/Main_program.java
+++ Source code/Main_program.java
@@ -4,11 +4,11 @@
     @author Ｖ字龍(Vdragon)
     @copyright Ｖ字龍放棄此專案的一切權利，將其釋出到公有領域(public domain)。 */
 
-/** @brief Main_program 物件類別(class)
+/** Main_program 物件類別(class)
 
     每個 Java 來源程式碼檔案至少要有一個 public 物件類別 */
 public class Main_program{
-	/** @brief public static void main() 為 Java 程式的進入點(entry point)子程式
+	/** public static void main() 為 Java 程式的進入點(entry point)子程式
 	 *  @param args 保存命令列參數的字串陣列，注意第一個命令列參數位於 0 索引值，本程式目前沒有實際用到此參數。 */
 	public static void main(String[] args){
 		/* 呼叫標準輸出(standard output)(System.out)物件的負責輸出字串的 println 方法(method) 以輸出「Java 程式設計世界哈囉！」字串 */
